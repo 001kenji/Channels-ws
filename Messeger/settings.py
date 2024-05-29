7310197372ff524f2099d1907cc23e16fe3a1251
BrianNjuguna <142185778+001kenji@users.noreply.github.com>
2024-05-29 10:06:03 +0300

    Update settings.py

--- Messeger/settings.py
+++ Messeger/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'channels',
     'channels_redis',
     'Chat',
+    "whitenoise.runserver_nostatic"
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
